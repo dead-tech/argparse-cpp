4a880b30b643e640e36564b4a989521a763b643c
dead-tech <dev.dead.tech@gmail.com>
2022-08-02 12:47:17 +0200

    :bug: argparse.hpp: Calling a builting wasn't terminating the program correctly

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile
 
 class Argparse(ConanFile):
     name = 'argparse-cpp'
-    version = '0.1.0'
+    version = '0.1.1'
     description = "Argument Parser in C++ inspired by python's argparse module"
     topics = ('conan', 'argparse', 'argparse-cpp', 'cli')
     url = 'https://github.com/dead-tech/argparse-cpp'
